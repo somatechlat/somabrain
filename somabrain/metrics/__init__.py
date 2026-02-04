f82add0be9a67a86d1e88135e4eef39206027128
somaplanet <cannachaincosta@gmail.com>
2026-02-04 06:15:16 -0500

    fix: export Milvus/Oak metrics from metrics module

--- somabrain/metrics/__init__.py
+++ somabrain/metrics/__init__.py
@@ -14,6 +14,17 @@ from somabrain.metrics.outbox_metrics import (
 from somabrain.metrics.memory_metrics import (
     record_memory_snapshot,
 )
+from somabrain.metrics.oak import (
+    MILVUS_INGEST_LAT_P95,
+    MILVUS_RECONCILE_MISSING,
+    MILVUS_RECONCILE_ORPHAN,
+    MILVUS_SEARCH_LAT_P95,
+    MILVUS_SEGMENT_LOAD,
+    MILVUS_UPSERT_FAILURE_TOTAL,
+    MILVUS_UPSERT_RETRY_TOTAL,
+    OPTION_COUNT,
+    OPTION_UTILITY_AVG,
+)
 
 __all__ = [
     "get_counter",
@@ -24,6 +35,15 @@ __all__ = [
     "report_outbox_pending",
     "report_outbox_processed",
     "record_memory_snapshot",
+    "MILVUS_INGEST_LAT_P95",
+    "MILVUS_RECONCILE_MISSING",
+    "MILVUS_RECONCILE_ORPHAN",
+    "MILVUS_SEARCH_LAT_P95",
+    "MILVUS_SEGMENT_LOAD",
+    "MILVUS_UPSERT_FAILURE_TOTAL",
+    "MILVUS_UPSERT_RETRY_TOTAL",
+    "OPTION_COUNT",
+    "OPTION_UTILITY_AVG",
     "advanced_math_metrics",
     "context_metrics",
     "math_metrics",
